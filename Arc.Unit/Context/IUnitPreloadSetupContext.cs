665c230aa46ca822d4c31e6ab1ad8205e4c1540b
archi-Doc <archidoc422@gmail.com>
2023-02-26 00:04:15 +0900

    .

---
+++ Arc.Unit/Context/IUnitPreloadSetupContext.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Arc.Unit;
+
+/// <summary>
+/// Contextual information used by Preload delegate and provided to <see cref="UnitBuilder"/>.
+/// </summary>
+public interface IUnitPreloadSetupContext
+{
+    public string UnitName { get; set; }
+
+    public string RootDirectory { get; set; }
+
+    public string DataDirectory { get; set; }
+
+    public UnitArguments Arguments { get; }
+
+    public void GetOptions<TOptions>(out TOptions options)
+        where TOptions : class;
+
+    public bool TryGetOptions<TOptions>([MaybeNullWhen(false)] out TOptions options)
+        where TOptions : class;
+}
