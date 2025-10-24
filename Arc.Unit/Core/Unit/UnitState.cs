d5c372ed97aca7bbbaba0675cc2596c4764b2347
archi-Doc <archidoc422@gmail.com>
2025-10-24 22:42:25 +0900

    .

---
+++ Arc.Unit/Core/Unit/UnitState.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+namespace Arc.Unit;
+
+/// <summary>
+/// Represents the lifecycle state of a unit.
+/// </summary>
+public enum UnitState : byte
+{
+    /// <summary>
+    /// The object is in its initial state and has not yet been activated.
+    /// </summary>
+    Initial,
+
+    /// <summary>
+    /// The object is active and valid.
+    /// </summary>
+    Active,
+
+    /// <summary>
+    /// The object has been disposed and cannot be reused.
+    /// </summary>
+    Rip,
+}
