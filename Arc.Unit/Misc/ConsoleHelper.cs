461b708d88d2e917ec1fb7071de3b22270769af8
archi-Doc <archidoc422@gmail.com>
2025-11-20 21:25:45 +0900

    .

--- Arc.Unit/Misc/ConsoleHelper.cs
+++ Arc.Unit/Misc/ConsoleHelper.cs
@@ -5,45 +5,45 @@ namespace Arc.Unit;
 public static class ConsoleHelper
 {
     public const ConsoleColor DefaultColor = (ConsoleColor)(-1);
-    public const string DefaultForegroundColor = "\x1B[39m\x1B[22m"; // reset to default foreground color
-    public const string DefaultBackgroundColor = "\x1B[49m"; // reset to the background color
+    public const string DefaultForegroundColor = "\u001b[39m\u001b[22m"; // reset to default foreground color
+    public const string DefaultBackgroundColor = "\u001b[49m"; // reset to the background color
 
-    public static ReadOnlySpan<char> EraseLineSpan => "\u001b[K";
+    public static ReadOnlySpan<char> EraseToEndOfLineSpan => "\u001b[K";
 
-    public static ReadOnlySpan<char> EraseLine2Span => "\u001b[2K";
+    public static ReadOnlySpan<char> EraseEntireLineSpan => "\u001b[2K";
 
     public static ReadOnlySpan<char> ResetSpan => "\u001b[0m";
 
-    public static ReadOnlySpan<char> SaveCursorSpan => "\x1b[s";
+    public static ReadOnlySpan<char> SaveCursorSpan => "\u001b[s";
 
-    public static ReadOnlySpan<char> RestoreCursorSpan => "\x1b[u";
+    public static ReadOnlySpan<char> RestoreCursorSpan => "\u001b[u";
 
-    public static ReadOnlySpan<char> HideCursorSpan => "\x1b[?25l";
+    public static ReadOnlySpan<char> HideCursorSpan => "\u001b[?25l";
 
-    public static ReadOnlySpan<char> ShowCursorSpan => "\x1b[?25h";
+    public static ReadOnlySpan<char> ShowCursorSpan => "\u001b[?25h";
 
-    public static ReadOnlySpan<char> SetCursorSpan => "\x1b["; // "\e[n;mH
+    public static ReadOnlySpan<char> SetCursorSpan => "\u001b["; // "\e[n;mH
 
     public static string GetForegroundColorEscapeCode(ConsoleColor color)
     {
         return color switch
         {
-            ConsoleColor.Black => "\x1B[30m",
-            ConsoleColor.DarkRed => "\x1B[31m",
-            ConsoleColor.DarkGray => "\x1B[90m",
-            ConsoleColor.DarkGreen => "\x1B[32m",
-            ConsoleColor.DarkYellow => "\x1B[33m",
-            ConsoleColor.DarkBlue => "\x1B[34m",
-            ConsoleColor.DarkMagenta => "\x1B[35m",
-            ConsoleColor.DarkCyan => "\x1B[36m",
-            ConsoleColor.Gray => "\x1B[37m",
-            ConsoleColor.Red => "\x1B[1m\x1B[31m",
-            ConsoleColor.Green => "\x1B[1m\x1B[32m",
-            ConsoleColor.Yellow => "\x1B[1m\x1B[33m",
-            ConsoleColor.Blue => "\x1B[1m\x1B[34m",
-            ConsoleColor.Magenta => "\x1B[1m\x1B[35m",
-            ConsoleColor.Cyan => "\x1B[1m\x1B[36m",
-            ConsoleColor.White => "\x1B[1m\x1B[37m",
+            ConsoleColor.Black => "\u001b[30m",
+            ConsoleColor.DarkRed => "\u001b[31m",
+            ConsoleColor.DarkGray => "\u001b[90m",
+            ConsoleColor.DarkGreen => "\u001b[32m",
+            ConsoleColor.DarkYellow => "\u001b[33m",
+            ConsoleColor.DarkBlue => "\u001b[34m",
+            ConsoleColor.DarkMagenta => "\u001b[35m",
+            ConsoleColor.DarkCyan => "\u001b[36m",
+            ConsoleColor.Gray => "\u001b[37m",
+            ConsoleColor.Red => "\u001b[1m\u001b[31m",
+            ConsoleColor.Green => "\u001b[1m\u001b[32m",
+            ConsoleColor.Yellow => "\u001b[1m\u001b[33m",
+            ConsoleColor.Blue => "\u001b[1m\u001b[34m",
+            ConsoleColor.Magenta => "\u001b[1m\u001b[35m",
+            ConsoleColor.Cyan => "\u001b[1m\u001b[36m",
+            ConsoleColor.White => "\u001b[1m\u001b[37m",
             _ => DefaultForegroundColor,
         };
     }
@@ -52,14 +52,14 @@ public static class ConsoleHelper
     {
         return color switch
         {
-            ConsoleColor.Black => "\x1B[40m",
-            ConsoleColor.DarkRed => "\x1B[41m",
-            ConsoleColor.DarkGreen => "\x1B[42m",
-            ConsoleColor.DarkYellow => "\x1B[43m",
-            ConsoleColor.DarkBlue => "\x1B[44m",
-            ConsoleColor.DarkMagenta => "\x1B[45m",
-            ConsoleColor.DarkCyan => "\x1B[46m",
-            ConsoleColor.Gray => "\x1B[47m",
+            ConsoleColor.Black => "\u001b[40m",
+            ConsoleColor.DarkRed => "\u001b[41m",
+            ConsoleColor.DarkGreen => "\u001b[42m",
+            ConsoleColor.DarkYellow => "\u001b[43m",
+            ConsoleColor.DarkBlue => "\u001b[44m",
+            ConsoleColor.DarkMagenta => "\u001b[45m",
+            ConsoleColor.DarkCyan => "\u001b[46m",
+            ConsoleColor.Gray => "\u001b[47m",
             _ => DefaultBackgroundColor,
         };
     }
