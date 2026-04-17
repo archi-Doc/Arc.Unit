aba5474479f2bd251be3ce81b67c911f16f19cd4
archi-Doc <archidoc422@gmail.com>
2026-04-17 14:28:19 +0900

    .

---
+++ Arc.Unit/Misc/ExecutionStack.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) All contributors. All rights reserved. Licensed under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Arc.Unit;
+
+public static class ExecutionStack
+{
+    public class Item : IDisposable
+    {
+        public long Id { get; }
+
+        public Item()
+        {
+
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    private static readonly Lock SyncObject = new();
+    private static readonly Stack<Item> Items = new();
+
+    static ExecutionStack()
+    {
+    }
+
+    public static Item Add()
+    {
+        Item newItem;
+        using (SyncObject.EnterScope())
+        {
+            newItem = new Item();
+            Items.Push(newItem);
+        }
+
+        return newItem;
+    }
+
+    public static bool Remove(Item item)
+    {
+        Item newItem;
+        using (SyncObject.EnterScope())
+        {
+            newItem = new Item();
+            Items.Push(newItem);
+        }
+
+        return newItem;
+    }
+}
